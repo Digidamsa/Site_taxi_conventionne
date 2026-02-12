268c6f2c26310c02ddab7242c5121b9e22470930
Digidamsa <damien.derhille@gmail.com>
2026-02-12 09:39:00 +0100

    Update HomePage.jsx
    3ème tentative de correction beug image page d'accueil
    test 1 : URL git hub
    test 2 : URL raw=true
    test 3 : URL avec token

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -100,7 +100,7 @@ const HomePage = () => {
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.8, delay: 0.4 }}
                 >
-                    <img class="w-full h-auto rounded-lg shadow-2xl" alt="Formulaire de prescription médicale de transport" src="https://github.com/Digidamsa/Site_taxi_conventionne/blob/main/images/prescription.jpg?raw=true" />
+                    <img class="w-full h-auto rounded-lg shadow-2xl" alt="Formulaire de prescription médicale de transport" src="https://raw.githubusercontent.com/Digidamsa/Site_taxi_conventionne/refs/heads/main/images/prescription.jpg?token=GHSAT0AAAAAADVLV2565IRD5HZOHBXMYS7Q2MNSFPA" />
                 </motion.div>
             </div>
           </div>
@@ -115,7 +115,7 @@ const HomePage = () => {
                     viewport={{ once: true }}
                     transition={{ duration: 0.8, delay: 0.2 }}
                 >
-                    <img class="w-full h-auto rounded-lg shadow-2xl" alt="Bannière Taxi Conventionné CPAM, VSL, Ambulance" src="https://github.com/Digidamsa/Site_taxi_conventionne/blob/main/images/Photo%20accueil.jpeg?raw=true" />
+                    <img class="w-full h-auto rounded-lg shadow-2xl" alt="Bannière Taxi Conventionné CPAM, VSL, Ambulance" src="https://raw.githubusercontent.com/Digidamsa/Site_taxi_conventionne/refs/heads/main/images/Photo%20accueil.jpeg?token=GHSAT0AAAAAADVLV256QTB533YGLEBBSXES2MNSGOA" />
                 </motion.div>
                 <motion.div
                     initial={{ opacity: 0, x: 50 }}
